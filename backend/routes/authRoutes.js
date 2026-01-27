7a1bebd0b202c9c2b8f108c3da67af6551ad4bcd
Aryan8776 <aryanchavan096@gmail.com>
2026-01-28 03:45:25 +0530

    Fix auth routes with admin login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
+
 const {
   registerUser,
-  loginUser
+  loginUser,
+  adminLogin
 } = require("../controllers/authController");
 
+const protect = require("../middleware/authMiddleware");
+
+// USER AUTH
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// ADMIN AUTH
 router.post("/admin-login", adminLogin);
 
 module.exports = router;
